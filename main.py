506ec7abf9bc897fd6c9cff06ef00fc3bf751cc2
Luca <54636488+iimrudy@users.noreply.github.com>
2021-06-07 19:20:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import requests
 
-
-username = "dentifriciosbiancante"
-
+username = "iim_rudy"
 
 if requests.get("https://t.me/" + username).text.find('<meta name="robots" content="noindex, nofollow">') >= 0:
     print("Username found")
-
 else:
     print("username not found")
